27fda4192f8358352dc98816c4efb5666d61b94f
MukiriJoel <jmukiri535@gmail.com>
2025-08-28 17:04:46 +0300

    update after presentation

--- app/music/[id]/page.tsx
+++ app/music/[id]/page.tsx
@@ -4,6 +4,7 @@ import {useParams, useSearchParams} from 'next/navigation';
 import MusicPlayer from './music-view';
 import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
+import MusicView from './music-view';
 
 const Page = () => {
     const params = useParams();
@@ -17,7 +18,7 @@ const Page = () => {
                 <NavBar/>
             </header>
             <main className="w-full min-h-screen">
-                <MusicPlayer id={id} bannerImage='/images/sandwich2.jpg' title = "Disko" artist = "Kodong Klan"   albumInfo = "Album • 1hr 45min • 10 Songs • Hiphop" audioSrc={"/audio/podcast.mp3"}/>
+                <MusicView id={id} bannerImage='/images/sandwich2.jpg' title = "Disko" artist = "Kodong Klan"   albumInfo = "Album • 1hr 45min • 10 Songs • Hiphop" audioSrc={"/audio/podcast.mp3"}/>
             </main>
             {/* <Footer/> */}
         </div>
